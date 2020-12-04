54a0661a957c0741a54a6268e56d6f1789b4fb62
JP <peacock.jared@gmail.com>
2020-12-04 10:39:04 -0800

    changed how an input dictionary is handled in metadata.Base, if a single layer dictionary is input it is assumed to be of the correct data type, there might be one fail case of where the user only input one key,value pair, it will throw an error

--- examples/metadata/make_example_files.py
+++ examples/metadata/make_example_files.py
@@ -10,19 +10,20 @@ from xml.etree import cElementTree as et
 from mth5 import metadata
 from pathlib import Path
 
-p = Path(r"c:\Users\jpeacock\Documents\GitHub\MTarchive\examples\metadata")
+p = Path(r"c:\Users\jpeacock\Documents\GitHub\mth5_test_data\metadata_files")
 
-style = "xml"
+style_list = ["json", "xml"]
 
-for c in ["Survey"]:  # , 'Station', 'Run', 'Channel', 'Electric', 'Magnetic']:
-    s = getattr(metadata, c)()
-    if style == "xml":
-        x = s.to_xml()
-        with open(p.joinpath("{0}_example.xml".format(c.lower())), "w") as fid:
-            fid.write(
-                minidom.parseString(et.tostring(x).decode()).toprettyxml(indent="    ")
-            )
-    elif style == "json":
-        jstr = s.to_json(structured=True)
-        with open(p.joinpath("{0}_example.json".format(c.lower())), "w") as fid:
-            fid.write(jstr)
+for style in style_list:
+    for c in ["Survey", 'Station', 'Run', 'Channel', 'Electric', 'Magnetic']:
+        s = getattr(metadata, c)()
+        if style == "xml":
+            x = s.to_xml(required=False)
+            with open(p.joinpath("{0}_example.xml".format(c.lower())), "w") as fid:
+                fid.write(
+                    minidom.parseString(et.tostring(x).decode()).toprettyxml(indent="    ")
+                )
+        elif style == "json":
+            jstr = s.to_json(nested=True, required=False)
+            with open(p.joinpath("{0}_example.json".format(c.lower())), "w") as fid:
+                fid.write(jstr)
