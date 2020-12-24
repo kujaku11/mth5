9afc06a50ed11b7f7aaf621c4463d8c1c7547dcf
JP <peacock.jared@gmail.com>
2020-12-23 17:37:19 -0800

    splitting datasets

---
+++ mth5/datasets/magnetic.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 23 17:34:32 2020
+
+:copyright: 
+    Jared Peacock (jpeacock@usgs.gov)
+
+:license: MIT
+
+"""
+# =============================================================================
+# Imports
+# =============================================================================
+from mth5.utils.helpers import inherit_doc_string
+from mth5.datasets import ChannelDataset
+
+# =============================================================================
+# Magnetic Channel
+# =============================================================================
+@inherit_doc_string
+class MagneticyDataset(ChannelDataset):
+    def __init__(self, group, **kwargs):
+
+        super().__init__(group, **kwargs)
