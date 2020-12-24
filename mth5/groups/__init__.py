bb7ef5f0df918d3fc3dd5149421329a03beea617
JP <peacock.jared@gmail.com>
2020-12-23 17:26:25 -0800

    splitting groups

--- mth5/groups/__init__.py
+++ mth5/groups/__init__.py
@@ -8,5 +8,6 @@ from .standards import StandardsGroup
 from .filters import FiltersGroup
 from .master_station import MasterStationGroup
 from .station import StationGroup
+from .run import RunGroup
 
-__all__ = ["BaseGroup", "SurveyGroup", "ReportsGroup", "StandardsGroup", "FiltersGroup", "StationGroup"]
+__all__ = ["BaseGroup", "SurveyGroup", "ReportsGroup", "StandardsGroup", "FiltersGroup", "StationGroup", "RunGroup"]
