694d0bff1bc5ee4f4b2cf64ba4ee60d4b409f4d2
JP <peacock.jared@gmail.com>
2021-10-31 21:50:26 -0700

    updated tests

---
+++ tests/version_1/test_build_from_experiment.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 13 13:45:27 2021
+
+:copyright: 
+    Jared Peacock (jpeacock@usgs.gov)
+
+:license: MIT
+
+"""
+# =============================================================================
+# Imports
+# =============================================================================
+
+import unittest
+from pathlib import Path
+
+from mth5 import mth5
+from mt_metadata.timeseries import Experiment
+from mt_metadata import MT_EXPERIMENT_SINGLE_STATION
+
+fn_path = Path(__file__).parent
+# =============================================================================
+#
+# =============================================================================
+mth5.helpers.close_open_files()
+
+
+class TestMTH5(unittest.TestCase):
+    def setUp(self):
+        self.maxDiff = None
+        self.fn = fn_path.joinpath("test.mth5")
+        self.mth5_obj = mth5.MTH5(file_version="0.1.0")
+        self.mth5_obj.open_mth5(self.fn, mode="w")
+        self.experiment = Experiment()
+        self.experiment.from_xml(fn=MT_EXPERIMENT_SINGLE_STATION)
+        self.mth5_obj.from_experiment(self.experiment)
+
+    def test_surveys(self):
+        survey = self.experiment.surveys[0]
+        self.assertEqual(survey, self.mth5_obj.survey_group.metadata)
+
+    def test_stations(self):
+        stations = self.experiment.surveys[0].stations
+        for station in stations:
+            h5_station = self.mth5_obj.get_station(station.id)
+            sd = station.to_dict(single=True)
+            sd.pop("hdf5_reference")
+            sd.pop("mth5_type")
+
+            h5_sd = h5_station.metadata.to_dict(single=True)
+            h5_sd.pop("hdf5_reference")
+            h5_sd.pop("mth5_type")
+
+            self.assertDictEqual(h5_sd, sd)
+
+    def test_runs(self):
+        runs = self.experiment.surveys[0].stations[0].runs
+        for run in runs:
+            h5_run = self.mth5_obj.get_run(
+                self.experiment.surveys[0].stations[0].id, run.id
+            )
+            sd = run.to_dict(single=True)
+            sd.pop("hdf5_reference")
+            sd.pop("mth5_type")
+
+            h5_sd = h5_run.metadata.to_dict(single=True)
+            h5_sd.pop("hdf5_reference")
+            h5_sd.pop("mth5_type")
+
+            self.assertDictEqual(h5_sd, sd)
+
+    def test_channels(self):
+        runs = self.experiment.surveys[0].stations[0].runs
+        for run in runs:
+            h5_run = self.mth5_obj.get_run(
+                self.experiment.surveys[0].stations[0].id, run.id
+            )
+            for channel in run.channels:
+                h5_channel = h5_run.get_channel(channel.component)
+
+                sd = channel.to_dict(single=True)
+                sd.pop("hdf5_reference")
+                sd.pop("mth5_type")
+
+                h5_sd = h5_channel.metadata.to_dict(single=True)
+                h5_sd.pop("hdf5_reference")
+                h5_sd.pop("mth5_type")
+
+                self.assertDictEqual(h5_sd, sd)
+
+    def test_filters(self):
+        exp_filters = self.experiment.surveys[0].filters
+
+        for key, value in exp_filters.items():
+            key = key.replace("/", " per ").lower()
+            sd = value.to_dict(single=True, required=False)
+            h5_sd = self.mth5_obj.filters_group.to_filter_object(key)
+            h5_sd = h5_sd.to_dict(single=True, required=False)
+            for k in sd.keys():
+                v1 = sd[k]
+                v2 = h5_sd[k]
+                if isinstance(v1, (float, int)):
+                    self.assertAlmostEqual(v1, float(v2), 5)
+                else:
+                    self.assertEqual(v1, v2)
+
+            # self.assertDictEqual(h5_sd, sd)
+
+    def tearDown(self):
+        self.mth5_obj.close_mth5()
+        self.fn.unlink()
---
+++ tests/version_1/test_make_mth5.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 26 12:46:55 2021
+
+@author: jpeacock
+"""
+import unittest
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+from mth5.clients.make_mth5 import MakeMTH5
+
+# =============================================================================
+# Test various inputs for getting metadata
+# =============================================================================
+
+
+class TestMakeMTH5(unittest.TestCase):
+    """
+    test a csv input to get metadata from IRIS
+
+    """
+
+    def setUp(self):
+
+        ZUCAS04LQ1 = [
+            "ZU",
+            "CAS04",
+            "",
+            "LQE",
+            "2020-06-02T19:00:00",
+            "2020-07-13T19:00:00",
+        ]
+        ZUCAS04LQ2 = [
+            "ZU",
+            "CAS04",
+            "",
+            "LQN",
+            "2020-06-02T19:00:00",
+            "2020-07-13T19:00:00",
+        ]
+        ZUCAS04BF1 = [
+            "ZU",
+            "CAS04",
+            "",
+            "LFE",
+            "2020-06-02T19:00:00",
+            "2020-07-13T19:00:00",
+        ]
+        ZUCAS04BF2 = [
+            "ZU",
+            "CAS04",
+            "",
+            "LFN",
+            "2020-06-02T19:00:00",
+            "2020-07-13T19:00:00",
+        ]
+        ZUCAS04BF3 = [
+            "ZU",
+            "CAS04",
+            "",
+            "LFZ",
+            "2020-06-02T19:00:00",
+            "2020-07-13T19:00:00",
+        ]
+        ZUNRV08LQ1 = [
+            "ZU",
+            "NVR08",
+            "",
+            "LQE",
+            "2020-06-02T19:00:00",
+            "2020-07-13T19:00:00",
+        ]
+        ZUNRV08LQ2 = [
+            "ZU",
+            "NVR08",
+            "",
+            "LQN",
+            "2020-06-02T19:00:00",
+            "2020-07-13T19:00:00",
+        ]
+        ZUNRV08BF1 = [
+            "ZU",
+            "NVR08",
+            "",
+            "LFE",
+            "2020-06-02T19:00:00",
+            "2020-07-13T19:00:00",
+        ]
+        ZUNRV08BF2 = [
+            "ZU",
+            "NVR08",
+            "",
+            "LFN",
+            "2020-06-02T19:00:00",
+            "2020-07-13T19:00:00",
+        ]
+        ZUNRV08BF3 = [
+            "ZU",
+            "NVR08",
+            "",
+            "LFZ",
+            "2020-06-02T19:00:00",
+            "2020-07-13T19:00:00",
+        ]
+        metadata_list = [
+            ZUCAS04LQ1,
+            ZUCAS04LQ2,
+            ZUCAS04BF1,
+            ZUCAS04BF2,
+            ZUCAS04BF3,
+            ZUNRV08LQ1,
+            ZUNRV08LQ2,
+            ZUNRV08BF1,
+            ZUNRV08BF2,
+            ZUNRV08BF3,
+        ]
+
+        self.csv_fn = Path().cwd().joinpath("test_inventory.csv")
+        self.mth5_path = Path().cwd()
+
+        self.stations = ["CAS04", "NVR08"]
+        self.channels = ["LQE", "LQN", "LFE", "LFN", "LFZ"]
+
+        self.make_mth5 = MakeMTH5(mth5_version="0.1.0")
+        self.make_mth5.client = "IRIS"
+        # Turn list into dataframe
+        self.metadata_df = pd.DataFrame(
+            metadata_list, columns=self.make_mth5.column_names
+        )
+        self.metadata_df.to_csv(self.csv_fn, index=False)
+
+        self.metadata_df_fail = pd.DataFrame(
+            metadata_list, columns=["net", "sta", "loc", "chn", "startdate", "enddate"]
+        )
+
+    def test_df_input_inventory(self):
+        inv, streams = self.make_mth5.get_inventory_from_df(
+            self.metadata_df, data=False
+        )
+        with self.subTest(name="stations"):
+            self.assertListEqual(
+                self.stations, [ss.code for ss in inv.networks[0].stations]
+            )
+        with self.subTest(name="channels_CAS04"):
+            self.assertListEqual(
+                self.channels, [ss.code for ss in inv.networks[0].stations[0].channels]
+            )
+        with self.subTest(name="channels_NVR08"):
+            self.assertListEqual(
+                self.channels, [ss.code for ss in inv.networks[0].stations[1].channels]
+            )
+
+    def test_csv_input_inventory(self):
+        inv, streams = self.make_mth5.get_inventory_from_df(self.csv_fn, data=False)
+        with self.subTest(name="stations"):
+            self.assertListEqual(
+                self.stations, [ss.code for ss in inv.networks[0].stations]
+            )
+        with self.subTest(name="channels_CAS04"):
+            self.assertListEqual(
+                self.channels, [ss.code for ss in inv.networks[0].stations[0].channels]
+            )
+        with self.subTest(name="channels_NVR08"):
+            self.assertListEqual(
+                self.channels, [ss.code for ss in inv.networks[0].stations[1].channels]
+            )
+
+    def test_fail_csv_inventory(self):
+        self.assertRaises(
+            ValueError,
+            self.make_mth5.get_inventory_from_df,
+            *(self.metadata_df_fail, self.make_mth5.client, False),
+        )
+
+    def test_fail_wrong_input_type(self):
+        self.assertRaises(
+            ValueError,
+            self.make_mth5.get_inventory_from_df,
+            *(("bad tuple", "bad_tuple"), self.make_mth5.client, False),
+        )
+
+    def test_fail_non_existing_file(self):
+        self.assertRaises(
+            IOError,
+            self.make_mth5.get_inventory_from_df,
+            *("c:\bad\file\name", self.make_mth5.client, False),
+        )
+
+    def test_make_mth5(self):
+        self.m = self.make_mth5.make_mth5_from_fdsnclient(
+            self.metadata_df, self.mth5_path, interact=True
+        )
+
+        with self.subTest(name="stations"):
+            self.assertListEqual(self.stations, self.m.station_list)
+
+        with self.subTest(name="CAS04_runs"):
+            self.assertListEqual(
+                ["a", "b", "c", "d"], self.m.get_station("CAS04").groups_list
+            )
+
+        for run in ["a", "b", "c", "d"]:
+            for ch in ["ex", "ey", "hx", "hy", "hz"]:
+                with self.subTest(name=f"has data CAS04.{run}.{ch}"):
+                    x = self.m.get_channel("CAS04", run, ch)
+                    self.assertFalse(np.all(x.hdf5_dataset == 0))
+
+        for run in ["a", "b", "c"]:
+            for ch in ["ex", "ey", "hx", "hy", "hz"]:
+                with self.subTest(name=f"has data NVR08.{run}.{ch}"):
+                    x = self.m.get_channel("NVR08", run, ch)
+                    self.assertFalse(np.all(x.hdf5_dataset == 0))
+
+        self.m.close_mth5()
+        self.m.filename.unlink()
+
+    def tearDown(self):
+        self.csv_fn.unlink()
+
+
+# =============================================================================
+# Run
+# =============================================================================
+if __name__ == "__main__":
+    unittest.main()
