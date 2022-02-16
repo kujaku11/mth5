62029a43015bdfeb78acb059003038f58614924a
JP <peacock.jared@gmail.com>
2022-02-15 18:41:55 -0800

    updated tests

--- tests/version_2/test_make_mth5.py
+++ tests/version_2/test_make_mth5.py
@@ -10,6 +10,7 @@ import pandas as pd
 import numpy as np
 
 from mth5.clients.make_mth5 import MakeMTH5
+from mth5.clients.fdsn import FDSN
 
 # =============================================================================
 # Test various inputs for getting metadata
@@ -24,178 +25,120 @@ class TestMakeMTH5(unittest.TestCase):
 
     def setUp(self):
 
-        ZUCAS04LQ1 = [
-            "ZU",
-            "CAS04",
-            "",
-            "LQE",
-            "2020-06-02T19:00:00",
-            "2020-07-13T19:00:00",
-        ]
-        ZUCAS04LQ2 = [
-            "ZU",
-            "CAS04",
-            "",
-            "LQN",
-            "2020-06-02T19:00:00",
-            "2020-07-13T19:00:00",
-        ]
-        ZUCAS04BF1 = [
-            "ZU",
-            "CAS04",
-            "",
-            "LFE",
-            "2020-06-02T19:00:00",
-            "2020-07-13T19:00:00",
-        ]
-        ZUCAS04BF2 = [
-            "ZU",
-            "CAS04",
-            "",
-            "LFN",
-            "2020-06-02T19:00:00",
-            "2020-07-13T19:00:00",
-        ]
-        ZUCAS04BF3 = [
-            "ZU",
-            "CAS04",
-            "",
-            "LFZ",
-            "2020-06-02T19:00:00",
-            "2020-07-13T19:00:00",
-        ]
-        ZUNRV08LQ1 = [
-            "ZU",
-            "NVR08",
-            "",
-            "LQE",
-            "2020-06-02T19:00:00",
-            "2020-07-13T19:00:00",
-        ]
-        ZUNRV08LQ2 = [
-            "ZU",
-            "NVR08",
-            "",
-            "LQN",
-            "2020-06-02T19:00:00",
-            "2020-07-13T19:00:00",
-        ]
-        ZUNRV08BF1 = [
-            "ZU",
-            "NVR08",
-            "",
-            "LFE",
-            "2020-06-02T19:00:00",
-            "2020-07-13T19:00:00",
-        ]
-        ZUNRV08BF2 = [
-            "ZU",
-            "NVR08",
-            "",
-            "LFN",
-            "2020-06-02T19:00:00",
-            "2020-07-13T19:00:00",
-        ]
-        ZUNRV08BF3 = [
-            "ZU",
-            "NVR08",
-            "",
-            "LFZ",
-            "2020-06-02T19:00:00",
-            "2020-07-13T19:00:00",
-        ]
-        metadata_list = [
-            ZUCAS04LQ1,
-            ZUCAS04LQ2,
-            ZUCAS04BF1,
-            ZUCAS04BF2,
-            ZUCAS04BF3,
-            ZUNRV08LQ1,
-            ZUNRV08LQ2,
-            ZUNRV08BF1,
-            ZUNRV08BF2,
-            ZUNRV08BF3,
-        ]
+        self.channels = ["LFE", "LFN", "LFZ", "LQE", "LQN"]
+        self.stations = ["CAY10", "CAS04", "NVR08"]
+        CAY10 = ["EM","CAY10", '2019-10-07T00:00:00', '2019-10-30T00:00:00']
+        CAS04 = ["ZU", "CAS04",  '2020-06-02T19:00:00', '2020-07-13T19:00:00']
+        NVR08 = ["ZU", "NVR08", '2020-06-02T19:00:00', '2020-07-13T19:00:00']
+        
+        request_list = []
+        for entry in [CAY10, CAS04, NVR08]:
+            for channel in self.channels:
+                request_list.append(
+                    [entry[0], entry[1], "", channel, entry[2], entry[3]]
+                )
 
         self.csv_fn = Path().cwd().joinpath("test_inventory.csv")
         self.mth5_path = Path().cwd()
 
-        self.stations = ["CAS04", "NVR08"]
-        self.channels = ["LQE", "LQN", "LFE", "LFN", "LFZ"]
-
         self.make_mth5 = MakeMTH5(mth5_version="0.2.0")
-        self.make_mth5.client = "IRIS"
+        self.make_mth5.save_path = self.mth5_path
+        self.make_mth5.interact = True
+        self.fdsn_object = FDSN()
+       
         # Turn list into dataframe
         self.metadata_df = pd.DataFrame(
-            metadata_list, columns=self.make_mth5.column_names
+            request_list, columns=self.fdsn_object.column_names
         )
         self.metadata_df.to_csv(self.csv_fn, index=False)
 
         self.metadata_df_fail = pd.DataFrame(
-            metadata_list, columns=["net", "sta", "loc", "chn", "startdate", "enddate"]
+            request_list, columns=["net", "sta", "loc", "chn", "startdate", "enddate"]
         )
 
     def test_df_input_inventory(self):
-        inv, streams = self.make_mth5.get_inventory_from_df(
+        inv, streams = self.fdsn_object.get_inventory_from_df(
             self.metadata_df, data=False
         )
         with self.subTest(name="stations"):
             self.assertListEqual(
-                self.stations, [ss.code for ss in inv.networks[0].stations]
+                [self.stations[0]], [ss.code for ss in inv.networks[0].stations]
+            )
+            
+        with self.subTest(name="stations"):
+            self.assertListEqual(
+                self.stations[1:], [ss.code for ss in inv.networks[1].stations]
+            )
+            
+        with self.subTest(name="channels_CAY10"):
+            self.assertListEqual(
+                self.channels, [ss.code for ss in inv.networks[1].stations[0].channels]
             )
         with self.subTest(name="channels_CAS04"):
             self.assertListEqual(
-                self.channels, [ss.code for ss in inv.networks[0].stations[0].channels]
+                self.channels, [ss.code for ss in inv.networks[1].stations[0].channels]
             )
         with self.subTest(name="channels_NVR08"):
             self.assertListEqual(
-                self.channels, [ss.code for ss in inv.networks[0].stations[1].channels]
+                self.channels, [ss.code for ss in inv.networks[1].stations[1].channels]
             )
 
     def test_csv_input_inventory(self):
-        inv, streams = self.make_mth5.get_inventory_from_df(self.csv_fn, data=False)
+        inv, streams = self.fdsn_object.get_inventory_from_df(self.csv_fn, data=False)
         with self.subTest(name="stations"):
             self.assertListEqual(
-                self.stations, [ss.code for ss in inv.networks[0].stations]
+                [self.stations[0]], [ss.code for ss in inv.networks[0].stations]
+            )
+            
+        with self.subTest(name="stations"):
+            self.assertListEqual(
+                self.stations[1:], [ss.code for ss in inv.networks[1].stations]
+            )
+            
+        with self.subTest(name="channels_CAY10"):
+            self.assertListEqual(
+                self.channels, [ss.code for ss in inv.networks[1].stations[0].channels]
             )
         with self.subTest(name="channels_CAS04"):
             self.assertListEqual(
-                self.channels, [ss.code for ss in inv.networks[0].stations[0].channels]
+                self.channels, [ss.code for ss in inv.networks[1].stations[0].channels]
             )
         with self.subTest(name="channels_NVR08"):
             self.assertListEqual(
-                self.channels, [ss.code for ss in inv.networks[0].stations[1].channels]
+                self.channels, [ss.code for ss in inv.networks[1].stations[1].channels]
             )
 
     def test_fail_csv_inventory(self):
         self.assertRaises(
             ValueError,
-            self.make_mth5.get_inventory_from_df,
-            *(self.metadata_df_fail, self.make_mth5.client, False),
+            self.fdsn_object.get_inventory_from_df,
+            *(self.metadata_df_fail, self.fdsn_object.client, False),
         )
 
     def test_fail_wrong_input_type(self):
         self.assertRaises(
             ValueError,
-            self.make_mth5.get_inventory_from_df,
-            *(("bad tuple", "bad_tuple"), self.make_mth5.client, False),
+            self.fdsn_object.get_inventory_from_df,
+            *(("bad tuple", "bad_tuple"), self.fdsn_object.client, False),
         )
 
     def test_fail_non_existing_file(self):
         self.assertRaises(
             IOError,
-            self.make_mth5.get_inventory_from_df,
-            *("c:\bad\file\name", self.make_mth5.client, False),
+            self.fdsn_object.get_inventory_from_df,
+            *("c:\bad\file\name", self.fdsn_object.client, False),
         )
 
     def test_make_mth5(self):
-        self.m = self.make_mth5.make_mth5_from_fdsnclient(
-            self.metadata_df, self.mth5_path, interact=True
-        )
+        self.m = self.make_mth5.from_fdsn_client(self.metadata_df)
 
         sg = self.m.get_survey("CONUS_South")
         with self.subTest(name="stations"):
-            self.assertListEqual(self.stations, sg.stations_group.groups_list)
+            self.assertListEqual(self.stations[1:], sg.stations_group.groups_list)
+            
+        sg = self.m.get_survey("Kansas_2017_Long_Period")
+        with self.subTest(name="stations"):
+            self.assertListEqual([self.stations[0]], sg.stations_group.groups_list)
 
         with self.subTest(name="CAS04_runs"):
             self.assertListEqual(
