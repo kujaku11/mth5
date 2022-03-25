13dbb4e59e367999467d4f385b805ed2e4818fe7
JP <peacock.jared@gmail.com>
2022-03-24 17:25:16 -0700

    updated tests

--- tests/version_2/test_transfer_function.py
+++ tests/version_2/test_transfer_function.py
@@ -71,46 +71,48 @@ class TestTFGroup(unittest.TestCase):
     def test_station_metadta(self):
         
         meta_dict = OrderedDict([('acquired_by.author', 'National Geoelectromagnetic Facility'),
-                     ('channels_recorded', ['ex', 'ey', 'hx', 'hy', 'hz']),
-                     ('data_type', 'mt'),
-                     ('fdsn.id', 'USMTArray.NMX20.2020'),
-                     ('geographic_name', 'Nations Draw, NM, USA'),
-                     ('id', 'NMX20'),
-                     ('location.datum', 'WGS84'),
-                     ('location.declination.epoch', '2020.0'),
-                     ('location.declination.model', 'WMM'),
-                     ('location.declination.value', 9.0899999999999999),
-                     ('location.elevation', 1940.05),
-                     ('location.latitude', 34.470528),
-                     ('location.longitude', -108.712288),
-                     ('orientation.angle_to_geographic_north', 0.0),
-                     ('orientation.method', None),
-                     ('orientation.reference_frame', 'geographic'),
-                     ('provenance.creation_time', '2021-03-17T14:47:44+00:00'),
-                     ('provenance.software.author', 'none'),
-                     ('provenance.software.name',
-                      'EMTF File Conversion Utilities 4.0'),
-                     ('provenance.software.version', None),
-                     ('provenance.submitter.author', 'Anna Kelbert'),
-                     ('provenance.submitter.email', 'akelbert@usgs.gov'),
-                     ('provenance.submitter.organization',
-                      'U.S. Geological Survey, Geomagnetism Program'),
-                     ('run_list', ['NMX20a', 'NMX20b']),
-                     ('time_period.end', '2020-10-07T20:28:00+00:00'),
-                     ('time_period.start', '2020-09-20T19:03:06+00:00'),
-                     ('transfer_function.coordinate_system', 'geopgraphic'),
-                     ('transfer_function.processing_parameters', ['{type: None}']),
-                     ('transfer_function.remote_references',
-                      ['NMX20b',
-                       'NMX20',
-                       'NMW20',
-                       'COR21',
-                       'NMY21-NMX20b',
-                       'NMX20',
-                       'UTS18']),
-                     ('transfer_function.runs_processed', ['NMX20a','NMX20b']),
-                     ('transfer_function.sign_convention', 'exp(+ i\\omega t)'),
-                     ('transfer_function.units', None)])
+                    ('channels_recorded', ['ex', 'ey', 'hx', 'hy', 'hz']),
+                    ('data_type', 'mt'),
+                    ('fdsn.id', 'USMTArray.NMX20.2020'),
+                    ('geographic_name', 'Nations Draw, NM, USA'),
+                    ('id', 'NMX20'),
+                    ('location.datum', 'WGS84'),
+                    ('location.declination.epoch', '2020.0'),
+                    ('location.declination.model', 'WMM'),
+                    ('location.declination.value', 9.0899999999999999),
+                    ('location.elevation', 1940.05),
+                    ('location.latitude', 34.470528),
+                    ('location.longitude', -108.712288),
+                    ('orientation.angle_to_geographic_north', 0.0),
+                    ('orientation.method', None),
+                    ('orientation.reference_frame', 'geographic'),
+                    ('provenance.creation_time', '2021-03-17T14:47:44+00:00'),
+                    ('provenance.software.author', 'none'),
+                    ('provenance.software.name',
+                     'EMTF File Conversion Utilities 4.0'),
+                    ('provenance.software.version', None),
+                    ('provenance.submitter.author', 'Anna Kelbert'),
+                    ('provenance.submitter.email', 'akelbert@usgs.gov'),
+                    ('provenance.submitter.organization',
+                     'U.S. Geological Survey, Geomagnetism Program'),
+                    ('run_list', ['NMX20a', 'NMX20b']),
+                    ('time_period.end', '2020-10-07T20:28:00+00:00'),
+                    ('time_period.start', '2020-09-20T19:03:06+00:00'),
+                    ('transfer_function.coordinate_system', 'geopgraphic'),
+                    ('transfer_function.id', 'NMX20'),
+                    ('transfer_function.processed_date', None),
+                    ('transfer_function.processing_parameters', ['{type: None}']),
+                    ('transfer_function.remote_references',
+                     ['NMX20b',
+                      'NMX20',
+                      'NMW20',
+                      'COR21',
+                      'NMY21-NMX20b',
+                      'NMX20',
+                      'UTS18']),
+                    ('transfer_function.runs_processed', ['NMX20a','NMX20b']),
+                    ('transfer_function.sign_convention', 'exp(+ i\\omega t)'),
+                    ('transfer_function.units', None)])
         
         self.assertDictEqual(meta_dict, self.tf_h5.station_metadata.to_dict(single=True))
         
@@ -158,6 +160,32 @@ class TestTFGroup(unittest.TestCase):
         
     def test_period(self):
         self.assertTrue((self.tf_obj.period == self.tf_h5.period).all())
+        
+    def test_tf_summary(self):
+        self.mth5_obj.tf_summary.clear_table()
+        self.mth5_obj.tf_summary.summarize()
+        
+        with self.subTest("test shape"):
+            self.assertEqual(self.mth5_obj.tf_summary.shape, (1,))
+            
+        true_dict = dict([
+            ("station", b"NMX20"),
+            ("latitude", 34.470528),
+            ("longitude", -108.712288),
+            ("elevation", 1940.05),
+            ("tf_id", b"NMX20"),
+            ("units", b"none"),
+            ("has_impedance", True),
+            ("has_tipper", True),
+            ("has_covariance", True),
+            ("period_min", 4.6545500000000004),
+            ("period_max", 29127.110000000001),])
+        for name in self.mth5_obj.tf_summary.dtype.names:
+            if "reference" in name:
+                continue
+            with self.subTest(f"test {name}"):
+                self.assertEqual(self.mth5_obj.tf_summary.array[name][0], 
+                                 true_dict[name])
         
     def tearDown(self):
         self.mth5_obj.close_mth5()
