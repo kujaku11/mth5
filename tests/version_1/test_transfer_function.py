0a2201fbe82b94a216a8c76f63597d3821199fae
JP <peacock.jared@gmail.com>
2022-03-31 18:31:53 -0700

    updating tests

--- tests/version_1/test_transfer_function.py
+++ tests/version_1/test_transfer_function.py
@@ -194,11 +194,10 @@ class TestTFGroup(unittest.TestCase):
 
         with self.subTest("test shape"):
             self.assertEqual(self.mth5_obj.tf_summary.shape, (1,))
-
         true_dict = dict(
             [
                 ("station", b"NMX20"),
-                ("survey", b"CONUS South"),
+                ("survey", b"CONUS_South"),
                 ("latitude", 34.470528),
                 ("longitude", -108.712288),
                 ("elevation", 1940.05),
