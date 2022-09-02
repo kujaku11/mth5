3f20824a1c38296e60d9191b7aeb191a5f8c03e5
JP <peacock.jared@gmail.com>
2022-09-02 14:32:34 -0700

    moving response

--- mth5/io/nims/__init__.py
+++ mth5/io/nims/__init__.py
@@ -1,7 +1,8 @@
 # package file
 from .gps import GPS
 from .header import NIMSHeader
+from .response_filters import Response
 from .nims import NIMS, read_nims
 
 
-__all__ = ["GPS", "NIMSHeader", "NIMS", "read_nims"]
+__all__ = ["GPS", "NIMSHeader", "Response", "NIMS", "read_nims"]
