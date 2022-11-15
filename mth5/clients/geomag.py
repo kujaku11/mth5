a1624163a7cb80767db857b1e1ef333ba40e4f15
JP <peacock.jared@gmail.com>
2022-11-14 16:31:53 -0800

    adding geomag client

---
+++ mth5/clients/geomag.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 13:58:44 2022
+
+@author: jpeacock
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+import requests
+import json
+
+import sys
+import platform
+import numpy as np
+
+from mth5 import __version__ as mth5_version
+
+from mt_metadata.utils.mttime import MTime
+
+# =============================================================================
+
+"https://geomag.usgs.gov/ws/data/?id=FRN&type=adjusted&elements=H&sampling_period=1&format=json&starttime=2020-06-02T19:00:00Z&endtime=2020-06-02T22:07:46Z"
+
+
+class GeomagClient:
+    """
+    Get geomagnetic data from observatories.
+
+    key words
+
+    - **observatory**: Geogmangetic observatory ID
+    - **type**: type of data to get 'adjusted'
+    - **start_time**: start date time to request UTC
+    - **end_time**: end date time to request UTC
+    - **elements**: components to get
+    - **sampling_period**: samples between measurements in seconds
+    - **format**: JSON or IAGA2002
+
+    .. seealso:: https://www.usgs.gov/tools/web-service-geomagnetism-data
+
+    """
+
+    def __init__(self, **kwargs):
+
+        self._base_url = r"https://geomag.usgs.gov/ws/data/"
+        self._timeout = 120
+        self._max_length = 172800
+
+        self._valid_observatories = [
+            "BDT",
+            "BOU",
+            "TST",
+            "BRW",
+            "BRT",
+            "BSL",
+            "CMO",
+            "CMT",
+            "DED",
+            "DHT",
+            "FRD",
+            "FRN",
+            "GUA",
+            "HON",
+            "NEW",
+            "SHU",
+            "SIT",
+            "SJG",
+            "TUC",
+            "USGS",
+            "BLC",
+            "BRD",
+            "CBB",
+            "EUA",
+            "FCC",
+            "IQA",
+            "MEA",
+            "OTT",
+            "RES",
+            "SNK",
+            "STJ",
+            "VIC",
+            "YKC",
+            "HAD",
+            "HER",
+            "KAK",
+        ]
+
+        self._valid_elements = [
+            "D",
+            "DIST",
+            "DST",
+            "E",
+            "E-E",
+            "E-N",
+            "F",
+            "G",
+            "H",
+            "SQ",
+            "SV",
+            "UK1",
+            "UK2",
+            "UK3",
+            "UK4",
+            "X",
+            "Y",
+            "Z",
+        ]
+
+        self._valid_sample_periods = [1, 60, 3600]
+        self._valid_output_formats = ["json", "iaga2002"]
+
+        self.type = "adjusted"
+        self.sampling_period = 1
+        self.elements = ["x", "y"]
+        self.format = "json"
+        self._timeout = 120
+        self.observatory = "FRN"
+        self._max_length = 172800
+
+    @property
+    def user_agent(self):
+        """
+        User agent for the URL request
+
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        encoding = sys.getdefaultencoding() or "UTF-8"
+        platform_ = (
+            platform.platform().encode(encoding).decode("ascii", "ignore")
+        )
+
+        return f"MTH5 v{mth5_version} ({platform_}, Python {platform.python_version})"
+
+    @property
+    def observatory(self):
+        return self._id
+
+    @observatory.setter
+    def observatory(self, value):
+        """
+        make sure value is in accepted list of observatories
+
+        :param value: DESCRIPTION
+        :type value: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        if not isinstance(value, str):
+            raise TypeError("input must be a string")
+
+        value = value.upper()
+        if value not in self._valid_observatories:
+            raise ValueError(
+                f"{value} not in accepted observatories see "
+                "https://www.usgs.gov/tools/web-service-geomagnetism-data "
+                "for more information."
+            )
+
+        self._id = value
+
+    @property
+    def elements(self):
+        return self._elements
+
+    @elements.setter
+    def elements(self, value):
+        """
+        make sure elements are in accepted elements
+        """
+
+        if isinstance(value, str):
+            if value.count(",") > 0:
+                value = [item.strip() for item in value.split(",")]
+
+        if not isinstance(value, list):
+            value = [value]
+
+        elements = []
+        for item in value:
+            if not isinstance(item, str):
+                raise TypeError(f"{item} in element list must be a string")
+            item = item.upper()
+            if item not in self._valid_elements:
+                raise ValueError(
+                    f"{item} is not an accepted element see "
+                    "https://www.usgs.gov/tools/web-service-geomagnetism-data "
+                    "for more information."
+                )
+            elements.append(item)
+
+        self._elements = elements
+
+    @property
+    def params(self):
+        """parameters for request"""
+        return {
+            "id": self.observatory,
+            "type": self.type,
+            "elements": "X,Y",
+            "sampling_period": "1",
+            "format": "json",
+            "starttime": "2020-06-02T19:00:00Z",
+            "endtime": "2020-06-02T22:07:46Z",
+        }
---
+++ tests/clients/test_geomag.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 14 15:33:20 2022
+
+@author: jpeacock
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+import unittest
+
+from mth5.clients.geomag import GeomagClient
+
+# =============================================================================
+
+
+class TestGeomagClient(unittest.TestCase):
+    def setUp(self):
+        self.client = GeomagClient()
+
+    def test_observatory(self):
+        self.client.observatory = "frn"
+        self.assertEqual("FRN", self.client.observatory)
+
+    def test_observatory_fail(self):
+        def set_observatory(value):
+            self.client.observatory = value
+
+        with self.subTest("ValueError"):
+            self.assertRaises(ValueError, set_observatory, "ten")
+
+        with self.subTest("TypeError"):
+            self.assertRaises(TypeError, set_observatory, 10)
+
+    def test_elements(self):
+        self.client.elements = "x"
+        self.assertListEqual(["X"], self.client.elements)
+
+    def test_elements_from_string(self):
+        self.client.elements = "x,y"
+        self.assertListEqual(["X", "Y"], self.client.elements)
+
+    def test_elements_fail_input(self):
+        def set_elements(value):
+            self.client.elements = value
+
+        with self.subTest("ValueError"):
+            self.assertRaises(ValueError, set_elements, "v")
+
+        with self.subTest("ValueError_list"):
+            self.assertRaises(ValueError, set_elements, ["x", "v"])
+
+        with self.subTest("TypeError"):
+            self.assertRaises(TypeError, set_elements, 10)
+
+        with self.subTest("TypeError_list"):
+            self.assertRaises(TypeError, set_elements, ["x", 10])
+
+
+# =============================================================================
+# Run
+# =============================================================================
+if __name__ == "__main__":
+    unittest.main()
