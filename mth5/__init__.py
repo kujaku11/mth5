bfb094a90897c8af48ff4f991f90b0b20b9b8f3d
JP <peacock.jared@gmail.com>
2023-01-17 21:31:54 -0800

    Bump version: 0.3.0 → 0.3.1

--- mth5/__init__.py
+++ mth5/__init__.py
@@ -14,7 +14,7 @@ from mth5.utils.mth5_logger import setup_logger, load_logging_config
 
 __author__ = """Jared Peacock"""
 __email__ = "jpeacock@usgs.gov"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 
 # =============================================================================
