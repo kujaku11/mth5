66b4b92103d9d72f41ee7980e0740c5ffcdfa11e
JP <peacock.jared@gmail.com>
2023-02-24 14:15:25 -0800

    prototyping FC storage

--- mth5/groups/fourier_coefficients.py
+++ mth5/groups/fourier_coefficients.py
@@ -1,46 +1,252 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 14 16:16:27 2023
+Created on Fri Feb 24 12:49:32 2023
 
 @author: jpeacock
 """
+
 # =============================================================================
 # Imports
 # =============================================================================
 import numpy as np
-import xarray as xr
+import pandas as pd
 import h5py
 
 from mth5.groups import BaseGroup
-from mth5.helpers import validate_name, from_numpy_type
 from mth5.utils.exceptions import MTH5Error
+from mth5.helpers import validate_name
 
+from mt_metadata.transfer_functions.fourier_coefficients import (
+    DecimationLevel,
+    Channel,
+    Window,
+)
 
 # =============================================================================
-class FourierCoefficientGroup(BaseGroup):
+
+
+class MasterFCGroup(BaseGroup):
     """
-    Object to hold a single transfer function estimation
+    Master group to hold various Fourier coefficient estimations of time series
+    data.
     """
 
     def __init__(self, group, **kwargs):
+
         super().__init__(group, **kwargs)
 
-        self._accepted_estimates = [
-            "transfer_function",
-            "transfer_function_error",
-            "inverse_signal_power",
-            "residual_covariance",
-            "impedance",
-            "impedance_error",
-            "tipper",
-            "tipper_error",
-        ]
+    @property
+    def fc_summary(self):
+        """
+        Summar of fourier coefficients
 
-        self._period_metadata = StatisticalEstimate(
-            **{
-                "name": "period",
-                "data_type": "float",
-                "description": "Periods at which transfer function is estimated",
-                "units": "samples per second",
-            }
-        )
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        pass
+
+    def add_fc_group(self, fc_name, fc_metadata=None):
+        """
+        Add a Fourier Coefficent group
+
+        :param fc_name: DESCRIPTION
+        :type fc_name: TYPE
+        :param fc_metadata: DESCRIPTION, defaults to None
+        :type fc_metadata: TYPE, optional
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        pass
+
+    def get_fc_group(self, fc_name):
+        """
+        Get Fourier Coefficient group
+
+        :param fc_name: DESCRIPTION
+        :type fc_name: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        pass
+
+    def remove_fc_group(self, fc_name):
+        """
+        Remove an FC group
+
+        :param fc_name: DESCRIPTION
+        :type fc_name: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+
+        pass
+
+
+class FCGroup(BaseGroup):
+    """
+    Holds a set of Fourier Coefficients based on a single set of configuration
+    parameters.
+
+    """
+
+    def __init__(self, group, fc_metadata=None, **kwargs):
+
+        super().__init__(group, group_metadata=fc_metadata, **kwargs)
+
+        self._default_subgroup_names = ["DecimationLevel"]
+
+    def initialize_group(self, **kwargs):
+        """
+        Initialize group by making a summary table and writing metadata
+
+        """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.write_metadata()
+
+        for group_name in self._default_subgroup_names:
+            self.hdf5_group.create_group(f"{group_name}")
+            m5_grp = getattr(self, f"{group_name.lower()}_group")
+            m5_grp.initialize_group()
+
+    def add_decimation_level(
+        self, decimation_level_name, decimation_level_metadata=None
+    ):
+        """
+        add a Decimation level
+
+        :param decimation_level_name: DESCRIPTION
+        :type decimation_level_name: TYPE
+        :param decimation_level_metadata: DESCRIPTION, defaults to None
+        :type decimation_level_metadata: TYPE, optional
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+
+        pass
+
+    def get_decimation_level(self, decimation_level_name):
+        """
+        Get a Decimation Level
+
+        :param decimation_level_name: DESCRIPTION
+        :type decimation_level_name: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        pass
+
+    def remove_decimation_level(self, decimation_level_name):
+        """
+        Remove decimation level
+
+        :param decimation_level_name: DESCRIPTION
+        :type decimation_level_name: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        pass
+
+
+class DecimationLevel(BaseGroup):
+    """
+    Holds a single decimation level
+    """
+
+    def __init__(self, group, **kwargs):
+
+        super().__init__(group, **kwargs)
+
+    def add_channel(self, channel_name, channel_metadata=None):
+        """
+        Add FC coefficients for a single channel
+
+        :param channel_name: DESCRIPTION
+        :type channel_name: TYPE
+        :param channel_metadata: DESCRIPTION, defaults to None
+        :type channel_metadata: TYPE, optional
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        pass
+
+    def get_channel(self, channel_name):
+        """
+        Get Fourier Coefficients for given channel
+
+        :param channel_name: DESCRIPTION
+        :type channel_name: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        pass
+
+    def remove_channel(self, channel_name):
+        """
+        Remove channel
+
+        :param channel_name: DESCRIPTION
+        :type channel_name: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        pass
+
+
+class FCChannel(BaseGroup):
+    """
+    Holds FC information for a single channel at a single decimation level.
+    """
+
+    def __init__(self, group, **kwargs):
+
+        super().__init__(group, **kwargs)
+
+    def add_fc_dataset(self, fc_datastet_name, fc_dataset_metadata=None):
+        """
+        Add a set of Fourier coefficients for a single channel at a single
+        decimation level for a processing run.
+
+        :param fc_datastet_name: DESCRIPTION
+        :type fc_datastet_name: TYPE
+        :param fc_dataset_metadata: DESCRIPTION, defaults to None
+        :type fc_dataset_metadata: TYPE, optional
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        pass
+
+    def get_fc_dataset(self, fc_dataset_name):
+        """
+        get an fc dataset
+
+        :param fc_dataset_name: DESCRIPTION
+        :type fc_dataset_name: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        pass
+
+    def remove_fc_dataset(self, fc_dataset_name):
+        """
+        remove an fc dataset
+
+        :param fc_dataset_name: DESCRIPTION
+        :type fc_dataset_name: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        pass
