248f634889983ca8ca47b2c9ad6da487f1e5f911
JP <peacock.jared@gmail.com>
2023-05-27 12:17:02 -0700

    updating tests

--- tests/version_1/test_transfer_function.py
+++ tests/version_1/test_transfer_function.py
@@ -32,7 +32,7 @@ class TestTFGroup(unittest.TestCase):
         self.mth5_obj.open_mth5(self.fn, mode="a")
 
         self.tf_obj = TF(TF_XML)
-        self.tf_obj.read_tf_file()
+        self.tf_obj.read()
 
         self.tf_group = self.mth5_obj.add_transfer_function(self.tf_obj)
         self.tf_h5 = self.mth5_obj.get_transfer_function(
@@ -43,6 +43,10 @@ class TestTFGroup(unittest.TestCase):
         meta_dict = OrderedDict(
             [
                 ("acquired_by.author", "National Geoelectromagnetic Facility"),
+                (
+                    "citation_dataset.authors",
+                    "Schultz, A., Pellerin, L., Bedrosian, P., Kelbert, A., Crosbie, J.",
+                ),
                 (
                     "citation_dataset.doi",
                     "doi:10.17611/DP/EMTF/USMTARRAY/SOUTH",
@@ -55,7 +59,7 @@ class TestTFGroup(unittest.TestCase):
                 ("citation_journal.doi", None),
                 (
                     "comments",
-                    "The USMTArray-CONUS South campaign was carried out through a cooperative agreement between\nthe U.S. Geological Survey (USGS) and Oregon State University (OSU). A subset of 40 stations\nin the SW US were funded through NASA grant 80NSSC19K0232.\nLand permitting, data acquisition, quality control and field processing were\ncarried out by Green Geophysics with project management and instrument/engineering\nsupport from OSU and Chaytus Engineering, respectively.\nProgram oversight, definitive data processing and data archiving were provided\nby the USGS Geomagnetism Program and the Geology, Geophysics and Geochemistry Science Centers.\nWe thank the U.S. Forest Service, the Bureau of Land Management, the National Park Service,\nthe Department of Defense, numerous state land offices and the many private landowners\nwho permitted land access to acquire the USMTArray data.",
+                    "copyright.acknowledgement:The USMTArray-CONUS South campaign was carried out through a cooperative agreement between\nthe U.S. Geological Survey (USGS) and Oregon State University (OSU). A subset of 40 stations\nin the SW US were funded through NASA grant 80NSSC19K0232.\nLand permitting, data acquisition, quality control and field processing were\ncarried out by Green Geophysics with project management and instrument/engineering\nsupport from OSU and Chaytus Engineering, respectively.\nProgram oversight, definitive data processing and data archiving were provided\nby the USGS Geomagnetism Program and the Geology, Geophysics and Geochemistry Science Centers.\nWe thank the U.S. Forest Service, the Bureau of Land Management, the National Park Service,\nthe Department of Defense, numerous state land offices and the many private landowners\nwho permitted land access to acquire the USMTArray data.; copyright.conditions_of_use:All data and metadata for this survey are available free of charge and may be copied freely, duplicated and further distributed provided that this data set is cited as the reference, and that the author(s) contributions are acknowledged as detailed in the Acknowledgements. Any papers cited in this file are only for reference. There is no requirement to cite these papers when the data are used. Whenever possible, we ask that the author(s) are notified prior to any publication that makes use of these data.\n While the author(s) strive to provide data and metadata of best possible quality, neither the author(s) of this data set, nor IRIS make any claims, promises, or guarantees about the accuracy, completeness, or adequacy of this information, and expressly disclaim liability for errors and omissions in the contents of this file. Guidelines about the quality or limitations of the data and metadata, as obtained from the author(s), are included for informational purposes only.; copyright.release_status:Unrestricted Release",
                 ),
                 ("country", ["USA"]),
                 ("datum", "WGS84"),
@@ -88,6 +92,10 @@ class TestTFGroup(unittest.TestCase):
             [
                 ("acquired_by.author", "National Geoelectromagnetic Facility"),
                 ("channels_recorded", ["ex", "ey", "hx", "hy", "hz"]),
+                (
+                    "comments",
+                    "description:Magnetotelluric Transfer Functions; primary_data.filename:NMX20b_NMX20_NMW20_COR21_NMY21-NMX20b_NMX20_UTS18.png; attachment.description:The original used to produce the XML; attachment.filename:NMX20b_NMX20_NMW20_COR21_NMY21-NMX20b_NMX20_UTS18.zmm; site.data_quality_notes.comments.author:Jade Crosbie, Paul Bedrosian and Anna Kelbert; site.data_quality_notes.comments.value:great TF from 10 to 10000 secs (or longer)",
+                ),
                 ("data_type", "mt"),
                 ("fdsn.id", "USMTArray.NMX20.2020"),
                 ("geographic_name", "Nations Draw, NM, USA"),
@@ -95,7 +103,7 @@ class TestTFGroup(unittest.TestCase):
                 ("location.datum", "WGS84"),
                 ("location.declination.epoch", "2020.0"),
                 ("location.declination.model", "WMM"),
-                ("location.declination.value", 9.0899999999999999),
+                ("location.declination.value", 9.09),
                 ("location.elevation", 1940.05),
                 ("location.latitude", 34.470528),
                 ("location.longitude", -108.712288),
@@ -122,7 +130,10 @@ class TestTFGroup(unittest.TestCase):
                 ("transfer_function.coordinate_system", "geopgraphic"),
                 ("transfer_function.id", "NMX20"),
                 ("transfer_function.processed_date", None),
-                ("transfer_function.processing_parameters", ["{type: None}"]),
+                (
+                    "transfer_function.processing_parameters",
+                    ["{remote_ref.type: Robust Multi-Station Reference}"],
+                ),
                 (
                     "transfer_function.remote_references",
                     [
@@ -227,7 +238,14 @@ class TestTFGroup(unittest.TestCase):
                     self.mth5_obj.tf_summary.array[name][0], true_dict[name]
                 )
 
-    @classmethod
-    def tearDownClass(self):
-        self.mth5_obj.close_mth5()
-        self.fn.unlink()
+    # @classmethod
+    # def tearDownClass(self):
+    #     self.mth5_obj.close_mth5()
+    #     self.fn.unlink()
+
+
+# =============================================================================
+# run
+# =============================================================================
+if __name__ == "__main__":
+    unittest.main()
