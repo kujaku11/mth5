835eabde05ba6f105d67e00797e528441277022d
JP <peacock.jared@gmail.com>
2023-06-21 11:52:18 -0700

    Update test_mth5_driver_v020.py

--- tests/notebooks/test_mth5_driver_v020.py
+++ tests/notebooks/test_mth5_driver_v020.py
@@ -23,15 +23,6 @@ from matplotlib import pyplot as plt
 
 from mt_metadata.transfer_functions.core import TF
 
-
-# get_ipython().run_line_magic("matplotlib", "widget")
-
-
-# ## Set the path to save files to as the current working directory
-
-# In[ ]:
-
-
 default_path = Path().cwd()
 
 
@@ -40,9 +31,6 @@ default_path = Path().cwd()
 # Here, we are setting the MTH5 file version to 0.2.0 so that we can have multiple surveys in a single file.  Also, setting the client to "IRIS".  Here, we are using `obspy.clients` tools for the request.  Here are the available [FDSN clients](https://docs.obspy.org/packages/obspy.clients.fdsn.html).
 #
 # **Note:** Only the "IRIS" client has been tested.
-
-# In[ ]:
-
 
 fdsn_object = FDSN(mth5_version="0.2.0")
 fdsn_object.client = "IRIS"
@@ -86,28 +74,13 @@ request_df
 #
 # Its helpful to be able to save the request as a CSV and modify it and use it later.  A CSV can be input as a request to `MakeMTH5`
 
-# In[ ]:
-
-
 request_df.to_csv(default_path.joinpath("fdsn_request.csv"))
-
 
 # ## Get only the metadata from IRIS
 #
 # It can be helpful to make sure that your request is what you would expect.  For that you can request only the metadata from IRIS.  The request is quick and light so shouldn't need to worry about the speed.  This returns a StationXML file and is loaded into an `obspy.Inventory` object.
 
-# In[ ]:
-
-
 inventory, data = fdsn_object.get_inventory_from_df(request_df, data=False)
-
-
-# Have a look at the Inventory to make sure it contains what is requested.
-
-# In[ ]:
-
-
-inventory
 
 
 # ## Make an MTH5 from a request
@@ -127,35 +100,14 @@ inventory
 #
 # make_mth5_object = MakeMTH5(mth5_version='0.2.0', interact=False)
 # mth5_filename = make_mth5_object.from_fdsn_client(request_df, client="IRIS")
-# ```
 
-# In[ ]:
-
-
-# get_ipython().run_cell_magic(
-#    "time",
-#    "",
-#    '\n'
 mth5_object = fdsn_object.make_mth5_from_fdsn_client(request_df, interact=False)
 print(f"Created {mth5_object}")
 # )
 
-
-# In[ ]:
-
-
 # open file already created
 mth5_object = MTH5()
 mth5_object.open_mth5("8P_CAS04_NVR08.h5")
-
-
-# ## Have a look at the contents of the created file
-
-# In[ ]:
-
-
-mth5_object
-
 
 # ## Channel Summary
 #
@@ -163,23 +115,15 @@ mth5_object
 #
 # **Note:** When a MTH5 file is close the table is resummarized so when you open the file next the `channel_summary` will be up to date. Same with the `tf_summary`.
 
-# In[ ]:
-
-
 mth5_object.channel_summary.clear_table()
 mth5_object.channel_summary.summarize()
 
 ch_df = mth5_object.channel_summary.to_dataframe()
-ch_df
-
 
 # ## Have a look at a station
 #
 # Lets grab one station `CAS04` and have a look at its metadata and contents.
 # Here we will grab it from the `mth5_object`.
-
-# In[ ]:
-
 
 cas04 = mth5_object.get_station("CAS04", survey="CONUS_South")
 cas04.metadata
@@ -188,13 +132,8 @@ cas04.metadata
 # ### Changing Metadata
 # If you want to change the metadata of any group, be sure to use the `write_metadata` method.  Here's an example:
 
-# In[ ]:
-
-
 cas04.metadata.location.declination.value = -13.5
 cas04.write_metadata()
-print(cas04.metadata.location.declination)
-
 
 # ## Have a look at a single channel
 #
@@ -203,32 +142,15 @@ print(cas04.metadata.location.declination)
 # 2. Get a channel from `mth5_object`
 # 3. Get a station first then get a channel
 #
-
-# In[ ]:
-
+# not sure about this error, catch it for now.
 try:
     ex = mth5_object.from_reference(ch_df.iloc[0].hdf5_reference).to_channel_ts()
-    print(ex)
-    
-    
-    # In[ ]:
-    
-    
+
     ex.channel_metadata
-    
     
     # ## Calibrate time series data
     # Most data loggers output data in digital counts.  Then a series of filters that represent the various instrument responses are applied to get the data into physical units.  The data can then be analyzed and processed. Commonly this is done during the processing step, but it is important to be able to look at time series data in physical units.  Here we provide a `remove_instrument_response` method in the `ChananelTS` object.  Here's an example:
-    
-    # In[ ]:
-    
-    
-    print(ex.channel_response_filter)
     ex.channel_response_filter.plot_response(np.logspace(-4, 1, 50))
-    
-    
-    # In[ ]:
-    
     
     ex.remove_instrument_response(plot=True)
 except ValueError as error:
@@ -242,111 +164,35 @@ except ValueError as error:
 # 2. Get a run from `mth5_object`
 # 3. Get a station first then get a run
 
-# In[ ]:
-
-
 run_from_reference = mth5_object.from_reference(
     ch_df.iloc[0].run_hdf5_reference
 ).to_runts(start=ch_df.iloc[0].start.isoformat(), n_samples=360)
-print(run_from_reference)
-
-
-# In[ ]:
-
 
 run_from_reference.plot()
-
-
-# ### Calibrate Run
-
-# In[ ]:
-
 
 calibrated_run = run_from_reference.calibrate()
 calibrated_run.plot()
 
 
 # ## Load Transfer Functions
-#
-# You can download the transfer functions for **CAS04** and **NVR08** from [IRIS SPUD EMTF](http://ds.iris.edu/spud/emtf).  This has already been done as EMTF XML format and will be loaded here.
+# You can download the transfer functions for **CAS04** and **NVR08** from [IRIS SPUD EMTF](http://ds.iris.edu/spud/emtf).  
+# This has already been done as EMTF XML format and will be loaded here. Not true, need to scrap from SPUD
 
-# In[ ]:
+try:
+    cas04_tf = r"USMTArray.CAS04.2020.xml"
+    nvr08_tf = r"USMTArray.NVR08.2020.xml"
+    
+    for tf_fn in [cas04_tf, nvr08_tf]:
+        tf_obj = TF(tf_fn)
+        tf_obj.read()
+        mth5_object.add_transfer_function(tf_obj)
 
-
-cas04_tf = r"USMTArray.CAS04.2020.xml"
-nvr08_tf = r"USMTArray.NVR08.2020.xml"
-
-
-# In[ ]:
-
-
-# from mt_metadata.transfer_functions.core import TF
-
-
-# In[ ]:
-
-
-for tf_fn in [cas04_tf, nvr08_tf]:
-    tf_obj = TF(tf_fn)
-    tf_obj.read()
-    mth5_object.add_transfer_function(tf_obj)
-
-
-# ### Have a look at the transfer function summary
-
-# In[ ]:
-
-
-mth5_object.tf_summary.summarize()
-tf_df = mth5_object.tf_summary.to_dataframe()
-tf_df
-
-
-# ### Plot the transfer functions using MTpy
-#
-# **Note:** This currently works on branch `mtpy/v2_plots`
-
-# In[ ]:
-
-## COMMENT OUT MTPY STUFF -- kk 20230604
-# from mtpy import MTCollection
-#
-#
-# # In[ ]:
-#
-#
-# mc = MTCollection()
-# mc.open_collection(r"8P_CAS04_NVR08")
-#
-#
-# # In[ ]:
-#
-#
-# pmr = mc.plot_mt_response(["CAS04", "NVR08"], plot_style="1")
-#
-#
-# # ## Plot Station locations
-# #
-# # Here we can plot station locations for all stations in the file, or we can give it a bounding box.  If you have internet access a basemap will be plotted using [Contextily](https://contextily.readthedocs.io/en/latest/).
-#
-# # In[ ]:
-#
-#
-# st = mc.plot_stations(pad=0.9, fig_num=5, fig_size=[6, 4])
-#
-#
-# # In[ ]:
-#
-#
-# st.fig.get_axes()[0].set_xlim((-121.9, -117.75))
-# st.fig.get_axes()[0].set_ylim((37.35, 38.5))
-# st.update_plot()
-
-
-# In[ ]:
+    mth5_object.tf_summary.summarize()
+    tf_df = mth5_object.tf_summary.to_dataframe()
+    tf_df
+except IOError as error:
+    print(error)
 
 
 mth5_object.close_mth5()
 
-
-# In[ ]:
