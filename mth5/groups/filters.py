45b9640d6f891f927722e6a790ab6b9f3af9df04
JP <peacock.jared@gmail.com>
2023-07-22 17:30:37 -0700

    Update filters.py

--- mth5/groups/filters.py
+++ mth5/groups/filters.py
@@ -43,26 +43,22 @@ class FiltersGroup(BaseGroup):
             self.zpk_group = ZPKGroup(self.hdf5_group.create_group("zpk"))
         except ValueError:
             self.zpk_group = ZPKGroup(self.hdf5_group["zpk"])
-
         try:
             self.coefficient_group = CoefficientGroup(
                 self.hdf5_group.create_group("coefficient")
             )
         except ValueError:
             self.coefficient_group = CoefficientGroup(self.hdf5_group["coefficient"])
-
         try:
             self.time_delay_group = TimeDelayGroup(
                 self.hdf5_group.create_group("time_delay")
             )
         except ValueError:
             self.time_delay_group = TimeDelayGroup(self.hdf5_group["time_delay"])
-
         try:
             self.fap_group = FAPGroup(self.hdf5_group.create_group("fap"))
         except ValueError:
             self.fap_group = FAPGroup(self.hdf5_group["fap"])
-
         try:
             self.fir_group = FIRGroup(self.hdf5_group.create_group("fir"))
         except ValueError:
@@ -99,35 +95,31 @@ class FiltersGroup(BaseGroup):
             try:
                 return self.zpk_group.from_object(filter_object)
             except ValueError:
-                self.logger.debug("group %s already exists", filter_object.name)
+                self.logger.debug(f"group {filter_object.name} already exists")
                 return self.zpk_group.get_filter(filter_object.name)
-
         elif filter_object.type in ["coefficient"]:
             try:
                 return self.coefficient_group.from_object(filter_object)
             except ValueError:
-                self.logger.debug("group %s already exists", filter_object.name)
+                self.logger.debug(f"group {filter_object.name} already exists")
                 return self.coefficient_group.get_filter(filter_object.name)
-
         elif filter_object.type in ["time_delay", "time delay"]:
             try:
                 return self.time_delay_group.from_object(filter_object)
             except ValueError:
-                self.logger.debug("group %s already exists", filter_object.name)
+                self.logger.debug(f"group {filter_object.name} already exists")
                 return self.time_delay_group.get_filter(filter_object.name)
-
         elif filter_object.type in ["fap", "frequency response table"]:
             try:
                 return self.fap_group.from_object(filter_object)
             except ValueError:
-                self.logger.debug("group %s already exists", filter_object.name)
+                self.logger.debug(f"group {filter_object.name} already exists")
                 return self.fap_group.get_filter(filter_object.name)
-
         elif filter_object.type in ["fir"]:
             try:
                 return self.fir_group.from_object(filter_object)
             except ValueError:
-                self.logger.debug("group %s already exists", filter_object.name)
+                self.logger.debug(f"group {filter_object.name} already exists")
                 return self.fir_group.get_filter(filter_object.name)
 
     def get_filter(self, name):
@@ -138,10 +130,9 @@ class FiltersGroup(BaseGroup):
         try:
             hdf5_ref = self.filter_dict[name]["hdf5_ref"]
         except KeyError:
-            msg = "Could not find %s in the filter dictionary"
-            self.logger.error(msg, name)
-            raise KeyError(msg, name)
-
+            msg = f"Could not find {name} in the filter dictionary"
+            self.logger.error(msg)
+            raise KeyError(msg)
         return self.hdf5_group[hdf5_ref]
 
     def to_filter_object(self, name):
@@ -155,7 +146,6 @@ class FiltersGroup(BaseGroup):
             msg = "Could not find %s in the filter dictionary"
             self.logger.error(msg, name)
             raise KeyError(msg, name)
-
         if f_type in ["zpk"]:
             return self.zpk_group.to_object(name)
         elif f_type in ["coefficient"]:
