e372a18d4b3a4952cd21e6cffc105b417ff04aa1
JP <peacock.jared@gmail.com>
2023-08-15 16:13:18 -0700

    Update __init__.py

--- mth5/io/zen/__init__.py
+++ mth5/io/zen/__init__.py
@@ -4,6 +4,7 @@ from .z3d_metadata import Z3DMetadata
 
 
 from .zen import Z3D, read_z3d
+from .coil_response import CoilResponse
 from .z3d_collection import Z3DCollection
 
 
@@ -12,6 +13,7 @@ __all__ = [
     "Z3DSchedule",
     "Z3DMetadata",
     "Z3D",
+    "CoilResponse",
     "read_z3d",
     "Z3DCollection",
 ]
