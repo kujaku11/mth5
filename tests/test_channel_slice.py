59ebf44a1c10d6c5935fa2751e8249dcb2a789df
JP <peacock.jared@gmail.com>
2023-09-15 16:50:54 -0700

    Create test_channel_slice.py

---
+++ tests/test_channel_slice.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 15 16:33:28 2023
+
+@author: jpeacock
+"""
+
+# =============================================================================
+# Imports
+# =============================================================================
+import unittest
+
+import numpy as np
+from mth5.mth5 import MTH5
+
+# =============================================================================
+
+
+class TestMTH5ChannelSlice(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        pass
