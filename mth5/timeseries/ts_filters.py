29dff5e1a5d20073f41a012da617a37231801585
kkappler <magnetotellurics@gmail.com>
2023-12-09 11:07:00 -0800

    remove references to filter correction operation

--- mth5/timeseries/ts_filters.py
+++ mth5/timeseries/ts_filters.py
@@ -563,8 +563,8 @@ class RemoveInstrumentResponse:
         # the complex response assumes frequencies are in reverse order and flip them on input
         # so we need to flip the complex reponse so it aligns with the fft.
         cr = self.channel_response.complex_response(f,
-                                                           filters_list=filters_to_remove,
-                                                           )[::-1]
+                                                    filters_list=filters_to_remove,
+                                                    )[::-1]
         # remove the DC term at frequency == 0
         cr[-1] = abs(cr[-2]) + 0.0j
 
@@ -579,16 +579,15 @@ class RemoveInstrumentResponse:
             data = self.apply_f_window(data)
             self.logger.debug(f"Step {step}: Applying {self.f_window} Frequency Window")
             step += 1
-        if self.channel_response.correction_operation == "divide":
-        #if operation == "divide":
+        if operation == "divide":
             # calibrate the time series, compute real part of fft, divide out
             # channel response, inverse fft
             calibrated_ts = np.fft.irfft(data / cr)[0 : self.ts.size]
             self.logger.debug(f"Step {step}: Removing Calibration by {operation}")
             step += 1
-        elif self.channel_response.correction_operation == "multiply":
-        # elif operation == "multiply":
-            logger.warning("It is unusual to apply the instrument response as it should already be in the data")
+        elif operation == "multiply":
+            msg = "It is unusual to apply the instrument response as it should already be in the data"
+            logger.warning("msg")
             # calibrate the time series, compute real part of fft, multiply out
             # channel response, inverse fft
             calibrated_ts = np.fft.irfft(data * cr)[0 : self.ts.size]
