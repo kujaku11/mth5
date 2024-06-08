96d995a9d2f72f7c54f563f7208cbbb3c05e000b
Karl N. Kappler <magnetotellurics@gmail.com>
2024-06-08 14:18:58 -0700

    passing local adhoc tests

--- mth5/utils/fc_tools.py
+++ mth5/utils/fc_tools.py
@@ -1,5 +1,7 @@
-from typing import Tuple, List, Union
 from dataclasses import dataclass
+from loguru import logger
+from typing import Tuple  #, Union
+import pandas as pd
 
 @dataclass
 class FCRunChunk():
@@ -14,6 +16,15 @@ class FCRunChunk():
     start: str = ""
     end: str = ""
     channels: Tuple[str] = ()
+
+    @property
+    def start_timestamp(self):
+        return pd.Timestamp(self.start)
+
+    @property
+    def end_timestamp(self):
+        return pd.Timestamp(self.end)
+
 
 
 def make_multistation_spectrogram(m, fc_run_chunks):
@@ -37,40 +48,56 @@ def make_multistation_spectrogram(m, fc_run_chunks):
 
     Station IDs must be unique.
 
-    TODO: Update this to take [ { station1:station1run,
-                                station2:station2run,
-                                ...
-                                stationN:stationNrun},
-                                ...
-                                {}
-                                ]
+
     Parameters
     ----------
-    m
-    station_ids
-    run_id
-    decimation_level_id
+    m: mth5.mth5.MTH5
+        The mth5 object to get the FCs from.
+    fc_run_chunks: iterable
+        Each element of this describes the run chunk to load.
 
     Returns
     -------
 
     """
-    for i_fcrc, fcrc in enumerate(fcrcs):
+    for i_fcrc, fcrc in enumerate(fc_run_chunks):
         station_obj = m.get_station(fcrc.station_id, fcrc.survey_id)
         station_fc_group = station_obj.fourier_coefficients_group
-        print(f"Available FC Groups for station {fcrc.station_id}: {station_fc_group.groups_list}")
+        logger.info(f"Available FC Groups for station {fcrc.station_id}: {station_fc_group.groups_list}")
         run_fc_group = station_obj.fourier_coefficients_group.get_fc_group(fcrc.run_id)
-        print(run_fc_group)
+        # print(run_fc_group)
         fc_dec_level = run_fc_group.get_decimation_level(fcrc.decimation_level_id)
-        fc_dec_level_xrds = fc_dec_level.to_xarray()
+        if fcrc.channels:
+            channels = list(fcrc.channels)
+        else:
+            channels = None
+
+        fc_dec_level_xrds = fc_dec_level.to_xarray(channels=channels)
         # could create name mapper dict from run_fc_group.channel_summary here if we wanted to.
+        if fcrc.start:
+            # TODO: Push this into the to_xarray command so we only access what we want
+            cond = fc_dec_level_xrds.time >= fcrc.start_timestamp
+            msg = (
+                f"trimming  {sum(~cond.data)} samples to {fcrc.start} "
+            )
+            logger.info(msg)
+            fc_dec_level_xrds = fc_dec_level_xrds.where(cond)
+            fc_dec_level_xrds = fc_dec_level_xrds.dropna(dim="time")
 
-        fc_dec_level = run_fc_group.get_decimation_level(decimation_level_id)
-        fc_dec_level_xrds = fc_dec_level.to_xarray()
+        if fcrc.end:
+            # TODO: Push this into the to_xarray command so we only access what we want
+            cond = fc_dec_level_xrds.time <= fcrc.end_timestamp
+            msg = (
+                f"trimming  {sum(~cond.data)} samples to {fcrc.end} "
+            )
+            logger.info(msg)
+            fc_dec_level_xrds = fc_dec_level_xrds.where(cond)
+            fc_dec_level_xrds = fc_dec_level_xrds.dropna(dim="time")
 
-        name_dict = {f"{x}": f"{station_id}_{x}" for x in fc_dec_level_xrds.data_vars}
-        print(name_dict)
-        if i_station == 0:
+
+        name_dict = {f"{x}": f"{fcrc.station_id}_{x}" for x in fc_dec_level_xrds.data_vars}
+
+        if i_fcrc == 0:
             output = fc_dec_level_xrds.rename_vars(name_dict=name_dict)
         else:
             fc_dec_level_xrds = fc_dec_level_xrds.rename_vars(name_dict=name_dict)
